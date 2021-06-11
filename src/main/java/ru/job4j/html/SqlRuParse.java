33e6fb889dacca1a036e9668fdede7a14a4c2d56
Borvenko Alena <alenaborvenko@gmail.com>
2021-06-11 21:02:55 +0700

    2.3. Загрузка деталей поста. [#285212]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -4,6 +4,7 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import ru.job4j.html.model.Post;
 import ru.job4j.utils.SqlRuDateTimeParser;
 
 import java.io.IOException;
@@ -11,20 +12,25 @@ import java.time.LocalDateTime;
 
 public class SqlRuParse {
     public static void main(String[] args) throws IOException {
-        for (int i = 1; i <= 5; i++) {
-            Document document = Jsoup.connect("https://www.sql.ru/forum/job-offers/" + i).get();
-            Elements row = document.select(".postslisttopic");
-            Elements rowDate = document.select(".altCol");
-            int altCol = 1;
-            for (Element el : row) {
-                String timeHTML = rowDate.get(altCol).text();
-                LocalDateTime time = new SqlRuDateTimeParser().parse(timeHTML);
-                System.out.println(time);
-                Element href = el.child(0);
-                System.out.println(href.attr("href"));
-                System.out.println(href.text());
-                altCol += 2;
+            Document document = Jsoup.connect(
+                    "https://www.sql.ru/forum/1325330/lidy-be-fe-senior-cistemnye-analitiki-qa-i-"
+                            + "devops-moskva-do-200t").get();
+            Elements msgBody = document.select(".msgBody");
+            Elements msgFooter = document.select(".msgFooter");
+            String name = document.select(".messageHeader").get(0).text();
+            int i = 1;
+            for (Element el : msgFooter) {
+                String href = el.baseUri();
+                Element data = msgBody.get(i);
+                LocalDateTime time = new SqlRuDateTimeParser().parse(el.text().split("\\[")[0]);
+                Post post = new Post(
+                        name,
+                        data.text(),
+                        href,
+                        time
+                );
+                System.out.println(post);
+                i += 2;
             }
-        }
     }
 }
