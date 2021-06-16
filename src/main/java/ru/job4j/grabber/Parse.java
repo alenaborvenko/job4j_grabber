3cd01bcc41741af5e349e7df3393bf7671a668e2
19208165 <554466Zxc>
2021-06-16 15:48:03 +0700

    4. Схема таблицы Post

--- src/main/java/ru/job4j/grabber/Parse.java
+++ src/main/java/ru/job4j/grabber/Parse.java
@@ -6,5 +6,6 @@ import java.util.List;
 
 public interface Parse {
     List<Post> list(String link);
+
     Post detail(String link);
 }
--- src/main/java/ru/job4j/grabber/Store.java
+++ src/main/java/ru/job4j/grabber/Store.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 public interface Store {
     void save(Post post);
+
     List<Post> getAll();
+
     Post findById(String id);
 }
