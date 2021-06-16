8fc3b8d7788ac6d8a0733df979813c5b0c242957
19208165 <554466Zxc>
2021-06-16 13:54:18 +0700

    2.3  Загрузка деталей поста

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -12,25 +12,21 @@ import java.time.LocalDateTime;
 
 public class SqlRuParse {
     public static void main(String[] args) throws IOException {
-            Document document = Jsoup.connect(
-                    "https://www.sql.ru/forum/1325330/lidy-be-fe-senior-cistemnye-analitiki-qa-i-"
-                            + "devops-moskva-do-200t").get();
-            Elements msgBody = document.select(".msgBody");
-            Elements msgFooter = document.select(".msgFooter");
-            String name = document.select(".messageHeader").get(0).text();
-            int i = 1;
-            for (Element el : msgFooter) {
-                String href = el.baseUri();
-                Element data = msgBody.get(i);
-                LocalDateTime time = new SqlRuDateTimeParser().parse(el.text().split("\\[")[0]);
-                Post post = new Post(
-                        name,
-                        data.text(),
-                        href,
-                        time
-                );
-                System.out.println(post);
-                i += 2;
-            }
+        Document document = Jsoup.connect(
+                "https://www.sql.ru/forum/1325330/lidy-be-fe-senior-cistemnye-analitiki-qa-i-"
+                        + "devops-moskva-do-200t").get();
+        Elements msgBody = document.select(".msgBody");
+        Element msgFooter = document.selectFirst(".msgFooter");
+        String name = document.select(".messageHeader").get(0).text();
+        String href = msgFooter.baseUri();
+        Element data = msgBody.get(1);
+        LocalDateTime time = new SqlRuDateTimeParser().parse(msgFooter.text().split("\\[")[0]);
+        Post post = new Post(
+                name,
+                data.text(),
+                href,
+                time
+        );
+        System.out.println(post);
     }
 }
