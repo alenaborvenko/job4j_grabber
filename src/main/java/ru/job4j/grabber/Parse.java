0acb3c2c0ddc91aec98e2e549616620b48607b39
19208165 <554466Zxc>
2021-06-16 14:46:18 +0700

    3. Архитектура проекта - Агрегатора Java Вакансий

--- src/main/java/ru/job4j/grabber/Parse.java
+++ src/main/java/ru/job4j/grabber/Parse.java
@@ -5,7 +5,6 @@ import ru.job4j.html.model.Post;
 import java.util.List;
 
 public interface Parse {
-
     List<Post> list(String link);
     Post detail(String link);
 }
---
+++ src/main/java/ru/job4j/grabber/Post.java
@@ -0,0 +1,89 @@
+package ru.job4j.grabber;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.StringJoiner;
+
+public class Post {
+    private int id;
+    private String name;
+    private String text;
+    private String link;
+    private LocalDateTime created;
+
+    public Post(String name, String text, String link, LocalDateTime created) {
+        this.name = name;
+        this.text = text;
+        this.link = link;
+        this.created = created;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Post post = (Post) o;
+        return id == post.id && Objects.equals(name, post.name) && Objects.equals(text, post.text)
+                && Objects.equals(link, post.link) && Objects.equals(created, post.created);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, text, link, created);
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", Post.class.getSimpleName() + "[", "]")
+                .add("id=" + id)
+                .add("name='" + name + "'")
+                .add("text='" + text + "'")
+                .add("link='" + link + "'")
+                .add("created=" + created)
+                .toString();
+    }
+}
