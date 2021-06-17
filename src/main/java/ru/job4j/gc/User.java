9d8b7f772070fddc29b8d3443d6f85f9fa36cfd3
19208165 <554466Zxc>
2021-06-17 11:17:01 +0700

    Демонстрация работы GC

---
+++ src/main/java/ru/job4j/gc/User.java
@@ -0,0 +1,33 @@
+package ru.job4j.gc;
+
+public class User {
+    private String name;
+    private String surname;
+
+    public User(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        System.out.printf("Removed %s %s%n", name, surname);
+        super.finalize();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+}
---
+++ src/main/java/ru/job4j/gc/UserGC.java
@@ -0,0 +1,12 @@
+package ru.job4j.gc;
+
+import static com.carrotsearch.sizeof.RamUsageEstimator.sizeOf;
+
+public class UserGC {
+    public static void main(String[] args) {
+        System.out.println(sizeOf(new User("-1", "N-1")) + " kb");
+        for (int i = 0; i < 1_000_000; i++) {
+            new User(String.valueOf(i), "N" + i);
+        }
+    }
+}
